0c94f58aa0aeceb401437637bc48b32d2aa602a3
Sourbomb14 <104939200+Sourbomb14@users.noreply.github.com>
2025-05-14 23:03:12 +0530

    Update job_model.py

--- models/job_model.py
+++ models/job_model.py
@@ -1,9 +1,8 @@
-def create_job(title, company, description, skills_required, stipend):
+def create_job(title, company, description, skills_required):
     return {
         "title": title,
         "company": company,
         "description": description,
         "skills_required": skills_required,
-        "stipend": stipend
     }
 
