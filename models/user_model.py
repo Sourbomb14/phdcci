939f4c832ea306fbbec5fbfafa8da908e9a1a8aa
Sourbomb14 <104939200+Sourbomb14@users.noreply.github.com>
2025-05-14 23:02:21 +0530

    Update user_model.py

--- models/user_model.py
+++ models/user_model.py
@@ -1,1 +1,10 @@
+def create_user(username, password, email, role, fullname, resume_path):
+    return {
+        "username": username,
+        "password": password,
+        "email": email,
+        "role": role,
+        "fullname": fullname,
+        "resume": resume_path
+    }
 
